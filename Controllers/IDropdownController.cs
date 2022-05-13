d1ed55e0220184e4d455f067664f2d1ccdd4dac5
AR-GE <AR-GE@DESKTOP-QTGR9T5>
2022-05-13 13:53:15 +0300

    message dropdown start

---
+++ Controllers/IDropdownController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminLTE_MVC.Controllers
+{
+    public interface IDropdownController
+    {
+        public IActionResult Index();
+    }
+}
---
+++ Controllers/MessageDropdownController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AdminLTE_MVC.Controllers
+{
+    public class MessageDropdownController : Controller, IDropdownController
+    {
+        public IActionResult Index()
+        {//Creating the List of SelectListItem, this list you can bind from the database.
+            List<SelectListItem> cities = new()
+            {
+                new SelectListItem { Value = "1", Text = "Latur" },
+                new SelectListItem { Value = "2", Text = "Solapur" },
+                new SelectListItem { Value = "3", Text = "Nanded" },
+                new SelectListItem { Value = "4", Text = "Nashik" },
+                new SelectListItem { Value = "5", Text = "Nagpur" },
+                new SelectListItem { Value = "6", Text = "Kolhapur" },
+                new SelectListItem { Value = "7", Text = "Pune" },
+                new SelectListItem { Value = "8", Text = "Mumbai" },
+                new SelectListItem { Value = "9", Text = "Delhi" },
+                new SelectListItem { Value = "10", Text = "Noida" }
+            };
+
+            //assigning SelectListItem to view Bag
+            ViewBag.cities = cities;
+            return View();
+        }
+    }
+}
