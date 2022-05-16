d54b6a9a49f7ae6d8be7032c86564c5a391ee3ec
Utku <mutkudoganay@gmail.com>
2022-05-16 12:34:28 +0300

    partial controller addition

---
+++ Controllers/AdminLTE/_MainNavigationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminLTE_MVC.Controllers
+{
+    public class _MainNavigationController : Controller
+    {
+        [Route("{controller=AdminLTE/_MainNavigation}/{action=Index}/{id?}")]
+        public IActionResult Index()
+        {
+            return View("AdminLTE/_MainNavigation");
+        }
+    }
+}
