4ade28381836825a8fb639d5128bdfdaf9a32740
Utku <mutkudoganay@gmail.com>
2022-05-16 14:54:50 +0300

    initial commit for partial view reload with c#

---
+++ Controllers/PartialViewController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminLTE_MVC.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        const string COLLAPSED = "left";
+        const string EXPANDED = "down";
+        public IActionResult MainNavigation()
+        {
+            if ((string?)ViewData["ArrowDirection"] == null)
+            {
+                Console.WriteLine("was null, now collapsed");
+                ViewData["ArrowDirection"] = COLLAPSED;
+            }
+            else
+            {
+                if ((string?)ViewData["ArrowDirection"] == EXPANDED)
+                {
+                    Console.WriteLine("was exp, now col");
+                    ViewData["ArrowDirection"] = COLLAPSED;
+                }
+                else
+                {
+                    Console.WriteLine("was col, now exp");
+                    ViewData["ArrowDirection"] = EXPANDED;
+                }
+            }
+            return PartialView("AdminLTE/_MainNavigation");
+        }
+    }
+}
---
+++ Models/MainNavigationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminLTE_MVC.Models
+{
+    public class MainNavigationViewModel
+    {
+        public string? ArrowDirection { get; set; }
+        public string? TreeviewHeight { get; set; }
+    }
+}
