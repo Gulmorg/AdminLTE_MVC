b59fb21f4c66adb26aa49992a72606f4e2dbd9e8
Utku <mutkudoganay@gmail.com>
2022-05-18 09:08:02 +0300

    unused files cleanup

--- Models/MainNavigationViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdminLTE_MVC.Models
-{
-    public class MainNavigationViewModel
-    {
-        public string ArrowDirection { get; set; } = "left";
-        public string TreeviewHeight { get; set; } = "150";
-    }
-}
