69a0632a2ee33cae7bfe3fbfd250d9d4a0a6e79b
Utku <mutkudoganay@gmail.com>
2022-05-31 15:03:20 +0300

    demo home page addition and profile relocation to top navigation

---
+++ Snmp/Target.cs
@@ -0,0 +1,21 @@
+﻿using Lextm.SharpSnmpLib;
+
+namespace AdminLTE_MVC.Snmp
+{
+    internal struct Target
+    {
+        public Target(string ip, string community, string oid) : this(ip, community, oid, VersionCode.V1) { }
+        public Target(string ip, string community, string oid, VersionCode version)
+        {
+            Ip = ip;
+            CommunityName = community;
+            Oid = oid;
+            VersionCode = version;
+        }
+
+        public string Ip { get; }
+        public string CommunityName { get; }
+        public string Oid { get; }
+        public VersionCode VersionCode { get; }
+    }
+}
