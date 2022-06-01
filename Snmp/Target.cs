2c8cf4f2e8daf3689d3efdb6d7c2fd8ec6b197d0
Utku <mutkudoganay@gmail.com>
2022-06-01 11:05:51 +0300

    Index view update

--- Snmp/Target.cs
+++ Snmp/Target.cs
@@ -4,6 +4,8 @@ namespace AdminLTE_MVC.Snmp
 {
     internal struct Target
     {
+        // Vutlan ctl units ".1.3.6.1.4.1.39052.1.3.1"
+
         public Target(string ip, string community, string oid) : this(ip, community, oid, VersionCode.V1) { }
         public Target(string ip, string community, string oid, VersionCode version)
         {
@@ -17,5 +19,7 @@ namespace AdminLTE_MVC.Snmp
         public string CommunityName { get; }
         public string Oid { get; }
         public VersionCode VersionCode { get; }
+
+        public Target SwitchOid(string oid) => new Target(Ip, CommunityName, oid, VersionCode); // TODO (test): Make sure GC gets rid of the old instance
     }
 }
