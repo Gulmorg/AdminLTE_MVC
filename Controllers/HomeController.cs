eca73571c091164d80d66d6a108272de3c579ad9
Utku <mutkudoganay@gmail.com>
2022-06-02 11:12:55 +0300

    TODO: Snmpwalk instead of creating a list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AdminLTE_MVC.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index()    // TODO: Snmp walk instead of creating a list
         {
             var targetTemplate = new Target(ip: "192.168.2.11", community: "public", oid: "1.3.6.1.4.1.39052.1.3.1.9", devId: "201003");
 
