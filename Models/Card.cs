d857fadb486e7ecb751ef75414dc8e79406fd4b6
Utku <mutkudoganay@gmail.com>
2022-06-08 18:04:25 +0300

    code injection guard bug, figure out how to generate html properly

--- Models/Card.cs
+++ Models/Card.cs
@@ -47,5 +47,32 @@
         public byte Position { get; set; }
         public string CardType { get; set; }
         public string CardTitle { get; set; }
+
+        public string Generate()
+        {
+            var output = $"<!-- Card 1 -->\n" +
+                         $"<div class=\"card card-info\">" +
+                         $"  <div class=\"card-header\">" +
+                         $"    <h3 class=\"card-title\"><i class=\"fas fa-thermometer\"></i> Kabin Sıcaklık</h3>" +
+                         $"    <div class=\"card-tools\">" +
+                         $"      <button type = \"button\" class=\"btn btn-tool\" data-card-widget=\"collapse\">" +
+                         $"        <i class=\"fas fa-minus\"></i>" +
+                         $"      </button>" +
+                         $"    </div>" +
+                         $"  </div>" +
+                         $"  <div class=\"card-body gauge-parent\">" +
+                         $"    <div class=\"text-center\">" +
+                         $"        @SnmpManager.GetValue(Model[1]) °C" +
+                         $"    </div>" +
+                         $"    <canvas id = \"gaugeOne\" style=\"min-height: 100%; height: 100%; max-height: 100%; max-width: 100%;\"></canvas>" +
+                         $"  </div>" +
+
+                         $"  <div class=\"card-footer text-center\">" +
+                         $"    More info<i class=\"fas fa-arrow-circle-right\"></i>" +
+                         $"  </div>" +
+                         $"</div>";
+
+            return output;
+        }
     }
 }
