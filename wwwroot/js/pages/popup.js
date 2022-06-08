837365875b8aefc7ac191d72a0c92a66f00f4aa6
Utku <mutkudoganay@gmail.com>
2022-06-08 15:05:06 +0300

    initial popup implementation

--- wwwroot/js/pages/popup.js
+++ wwwroot/js/pages/popup.js
@@ -1,19 +1,5 @@
 $(document).ready(function () {
-    $('#dialog').dialog({
-        autoOpen: false
-    })
     $('#btn').click(function () {
-        $("#dialog").dialog({
-            modal: true,
-            height: 600,
-            width: 500,
-
-            buttons: {
-                Accept: function () {
-                    $(this).dialog("close");
-                }
-            }
-        });
-        $('#dialog').dialog('open');
+        $('#dialog').modal('show')
     });
 });
