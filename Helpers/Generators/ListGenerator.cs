1aaa9e060fbea955079f2dae7635ce268a493bbd
Utku <mutkudoganay@gmail.com>
2022-06-15 17:31:37 +0300

    modal dropdown population via snmp name list

--- Helpers/Generators/ListGenerator.cs
+++ Helpers/Generators/ListGenerator.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.AspNetCore.Html;
+﻿using AdminLTE_MVC.Models.Dashboard;
+using AdminLTE_MVC.Snmp;
+using Microsoft.AspNetCore.Html;
 
 namespace AdminLTE_MVC.Helpers.Generators
 {
     public class ListGenerator
     {
+        public ListGenerator(List<Target> targets)
+        {
+            foreach (var target in targets)
+            {
+                titles.Add(SnmpManager.GetValue(target.ChangeOid(SnmpManager.SetOid("Name"))).ToString());
+            }
+        }
+
+        private List<string> titles = new();
+
+
         public IHtmlContent Generate()
         {
-            var test = "test";
-            var output = $"<option>{test}</option>  " +
-                         $"<option>{test}</option>  " +
-                         $"<option>{test}</option>  " +
-                         $"<option>{test}</option>  " +
-                         $"<option>{test}</option>  ";
+            var output = string.Empty;
+            foreach (var title in titles)
+            {
+                output += $"<option>{title}</option>";
+            }
             return new HtmlString(output);
         }
     }
